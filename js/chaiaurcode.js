ac2bcfd47ba7c0807a814d62fc15975c4778f765
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2025-02-22 22:24:58 +0530

    add day3 js notes (DOM +  methods)

--- js/chaiaurcode.js
+++ js/chaiaurcode.js
@@ -12,7 +12,21 @@
  * 11 - number methods
  * 12 - date and time methods
  * 13 - array methods
- * 14 - **INCOMPLETE**
+ * 14 - object methods
+ * 15 - destructuring and JSON
+ * 16 - functions
+ * 17 - scope
+ * 18 - this keyword - current context for classes and objects
+ * 19 - arrow function
+ * 20 - IIFE
+ * 21 - execution context
+ * 22 - truthy and falsy values
+ * 23 - Higher order array loops
+ * 24 - map,reduce,filter
+ * 25 - DOM (Document Object Model) - small stackblitz projects
+ * 26 - events
+ * 27 - Async JS
+ * 28 - timers
  */
 
 let variable1 = "firstVal";
@@ -38,13 +52,81 @@ console.table([variable1, variable2, variable3, variable4]);
 
 // use parenthesis, readability and clean code is very important
 
-// use `` - string interpolation
+// symbol as a key in an object
+const sym1 = Symbol("key1");
+const obj = { normal: "normal val", [sym1]: "value1" }; // just sym1 is stored as a string
+console.log(obj[sym1]);
+console.log(obj["normal"]);
+// console.log(obj[normal]); // ?
+console.log(obj.normal);
 
-const firstName = new String("Harvey");
-console.log(firstName.__proto__);
+// functions
+function multipleArgs(arg1, arg2, ...args) {
+    // arg1,arg2-variables, args-array
+    console.log(args);
+}
 
-// TODO : explore string methods
-// TODO : explore number methods
-// TODO : explore math methods
-// TODO : explore date methods
-// TODO : explore array methods
+console.log(addTwoNumbers(1, 2));
+// console.log(expression(1, 2)); // gives you error, hoisting
+function addTwoNumbers(arg1, arg2) {
+    return arg1 + arg2;
+}
+const expression = function (arg1, arg2) {
+    return arg1 + arg2;
+};
+
+// we do not have access to `this` in function and arrow function
+// console.log(this); // try out in all locations
+// global object in browser - window
+// global object in node - {}
+
+// arrow function -
+// 1. implicit return
+// 2. explicit return for { } block
+// 3. {{}} if you want to return an object
+
+// IIFE - Immediately Invoked Function Expression
+// 1. the function which executes immediately
+// 2. to remove global scope pollution from variables
+// 3. ; is important if you want 2 IIFEs in 1 file
+(function iife(arg) {})("param"); // named IIFE
+((arg) => {})("param"); // unnamed or simple IIFE
+
+// Javascript execution context
+/**
+ * global execution context (this) - different for browser,node,deno,bun,...
+ * function execution context -
+ * eval execution context -
+ * 2 phases - memory creation phase, execution phase
+ * new exection context - new variable environment + exection thread
+ * inspect->sources->snippets - visualise all this process
+ *
+ */
+
+// falsy values - false,0,-0,BigInt On,"",null,undefined,NaN
+// truthy values - (remaining), "0",'false'," ",[],{},function(){}
+// arrays and objects - check for length
+// == vs ===
+
+// Nullish Coalescing Operator (??) - only for null undefined
+// first value which you get is assigned
+let val1 = null ?? "first val" ?? "second val";
+let val2 = undefined ?? "first val" ?? "second val";
+
+// ["","",""], [{},{},{}] - general JSON data
+
+// Events;
+/**
+ * use reference of a function for onClick
+ * event propogation - bubbling(inside to outside) and capturing(ancestor to child)
+ * event listener
+ */
+
+// Stackblitz DOM projects
+/**
+ * color changer
+ * BMI calculator
+ * digital running clock
+ * Guess the number
+ *
+ */
--- js/loki_js_docs.js
+++ js/loki_js_docs.js
@@ -1,16 +1,65 @@
 // TODO : research more about use strict
 // TODO : have fun - try out all possible conversions and comparisions
-// TODO : explore string methods
+
+// TODO : explore string methods and master them
+/**
+ * new String - is an object
+ * use backtick(`) - string interpolation
+ * __proto__ -
+ * charAt() -
+ * indexOf() -
+ * length -
+ * substring(start,end) - ignores -ve values
+ * slice(start,end) - supports -ve values
+ * trim - whitespace and line terminator for both left and right
+ * replace() -
+ * includes() -
+ * split() -
+ */
+
 // TODO : explore number methods
+/**
+ * toString() - number to string
+ * toFixed(precision) - decimal
+ * toPrecision(digits) - returns string
+ * toLocaleString() - default is international
+ *
+ */
+
 // TODO : explore math methods
-// TODO : explore date methods
+/**
+ * abs() -
+ * round() -
+ * floor() -
+ * ceil() -
+ * sqrt() -
+ * random() - 0 to 1
+ */
+
+// TODO : explore date and time methods
+/**
+ * Temporal
+ * new Date(yyyy,mm,dd,hh,mimi,) or ("YYYY-MM-DD") or ("DD-MM-YYYY") - mm starts from 0
+ * typeof - object
+ * toString() -
+ * toDateString() -
+ * toLocaleString('default',{}) -
+ * now() - timestamp in milliseconds(ms)
+ * getTime() - timestamp in milliseconds(ms)
+ * getDay() -
+ * getMonth()+1 -
+ */
 
 // TODO : explore array methods
 /**
+ * copies - shallow copy - same ref point
+ * new Array(1,2,3,...)
  * push() - push_back()
  * pop() - pop_back()
  * unshift() - push_front()
  * shift() - pop_front()
+ * includes() - true or false
+ * indexOf() - first occurence else -1
  * delete arr[pos] -
  * does not give "index out of bounds"
  * splice() - used for deleting and replacing at any pos
@@ -19,17 +68,28 @@
  * join() - default is ','
  * split() -
  * concat() - arr1+arr2
- * ... - spread operator - [...arr1,...arr2]
+ * ... - rest or spread operator - [...arr1,...arr2]
+ * flat(level or depth) - can be Infinity
+ * isArray() -
+ * from() - ignores if not mentioned properly such as keys or values for an object
+ *
  */
 
 // TODO : explore object methods
 /**
+ * singleton - only one instance, when created using constructor (create())
+ * object literals - {}
  * create() - instantiates or copies
  * keys() -
  * values() -
  * for in -
  * delete -
  * hasOwnProperty() -
+ * freeze() - can't be modified
+ * assign({},obj1,obj2,...) - good practice
+ * {...obj1,...obj2} - spread operator
+ * keys(obj) - returns an array
+ * entries(obj) - returns an array of key value arrays
  *
  */
 
@@ -38,4 +98,44 @@
  * stringify() - obj to str
  * parse() - str to obj
  *
+ */
+
+// TODO : Higher Order Loops
+/**
+ * of - for(const iterator of object) - object can be array,string,object
+ * map - new Map() - not iterable- order is maintained with unique values - for(const [key,val] of map) -
+ * in - for(const key in object) - keys for array are indices
+ * forEach - forEach(function(item){}) = forEach((item,index,arr)=>{}) = forEach(funcRef) - does not return anything
+ * filter - filter(item => condition) or filter(item => {return condition}) - the same can also be achieved using forEach
+ * map - map(item => returnVal) or map(item => {return returnVal}) - returns array - chaining
+ * reduce - reduce((acc,curVal) => acc+curVal, initialVal) - log to understand the functionality
+ *
+ */
+
+// TODO : explore event
+/**
+ * addEventListener(eventName,func,false) - eventNames(keyboard, mouse, drag and drop - type, timestamp, preventDefault, target, currentTarget, toElement, srcElement, clientX, clientX, screenX, screenY, altkey, ctrlkey, shiftkey, keyCode(typing test)), stopPropogation - 3rd parameter (true->capturing, bubbling->false)
+ * attackEvent() -
+ * event object -
+ *
+ */
+
+// TODO : understand asynchronous JS
+/**
+ * default javascript (standalone JS engine) is synchronous and single thread
+ * blocking (synchronous) vs non-blocking code (asynchronous)
+ * JS Engine
+ * Web API / node API
+ * Register callback
+ * Task Queue
+ * High Priority Queue or Promise Queue
+ * Event Loop
+ */
+
+// TODO : explore timers
+/**
+ * setTimeout()
+ * clearTimeout()
+ * setInterval(func,time,params)
+ * clearInterval()
  */

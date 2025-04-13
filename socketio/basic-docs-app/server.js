25dbd03fec0c620dd22a2dcef60d2e4e05840358
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2025-04-13 22:01:44 +0530

    Socket IO (basic) - understand 'defer' vs 'DOMContentLoaded' clearly - create a basic chat app as per Socket IO docs

---
+++ socketio/basic-docs-app/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('server running at http://localhost:3000');
+});
